cdf8acac28e5b4bc5c347f289dff0cf41be707da
AimjGuytidy <aimjguy@gmail.com>
2023-11-30 11:46:29 +0200

    chapter 2

---
+++ hello_p.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+inline void keep_windy() { chr ch; cin >> ch; }
+int main()
+{
+	cout << "Hello, programming!\nHere we go!";
+	keep_windy();
+	return 0;
+}
