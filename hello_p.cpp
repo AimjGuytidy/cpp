dcefe3479e5a0e6e2dd92157f2ad776ca25c72a3
AimjGuytidy <aimjguy@gmail.com>
2023-11-30 13:23:38 +0200

    Objects, types and values

--- hello_p.cpp
+++ hello_p.cpp
@@ -4,10 +4,23 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-inline void keep_windy() { chr ch; cin >> ch; }
+inline void keep_windy() { char ch; cin >> ch; }
+//int main()
+//{
+//	cout << "Hello, programming!\nHere we go!";
+//	keep_windy();
+//	return 0;
+//}
+
+// input
+
 int main()
 {
-	cout << "Hello, programming!\nHere we go!";
-	keep_windy();
-	return 0;
+	cout << "Please enter your first name:\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Hello," << first_name << "!\n";
 }
+
+// variables
+
