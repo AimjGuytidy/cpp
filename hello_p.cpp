e486d797dc131e09dd5582881a8d070518cee0ef
AimjGuytidy <aimjguy@gmail.com>
2023-11-30 15:10:34 +0200

    Objects, types and values

--- hello_p.cpp
+++ hello_p.cpp
@@ -14,13 +14,32 @@ inline void keep_windy() { char ch; cin >> ch; }
 
 // input
 
-int main()
-{
-	cout << "Please enter your first name:\n";
-	string first_name;
-	cin >> first_name;
-	cout << "Hello," << first_name << "!\n";
-}
+//int main()
+//{
+//	cout << "Please enter your first name:\n";
+//	string first_name; //definition
+//	cin >> first_name;
+//	cout << "Hello," << first_name << "!\n";
+//}
 
 // variables
 
+// int:integer
+// double: a floating-point number
+// char: an individual character enclosed in single quotes
+// string: sequence of characters delimited by double quotes
+// bool: either true or false
+
+
+// input and type
+
+int main()
+{
+	cout << "Hello, Enter your name and age:\n";
+	string first_name; // string variable definition
+	int age; // integer variable definition
+	cin >> first_name; // reading a string
+	cin >> age; // reading an integer
+	cout << "Hello, " << first_name << ", your age is " << age << " !\n";
+	return 0;
+}
