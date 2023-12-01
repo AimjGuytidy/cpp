d3ec169399397ace0a47615c3f92b1a884dd22fe
AimjGuytidy <aimjguy@gmail.com>
2023-12-01 09:18:15 +0200

    Objects, types and values

--- hello_p.cpp
+++ hello_p.cpp
@@ -33,13 +33,36 @@ inline void keep_windy() { char ch; cin >> ch; }
 
 // input and type
 
+//int main()
+//{
+//	cout << "Hello, Enter your name and age:\n";
+//	string first_name; // string variable definition
+//	int age; // integer variable definition
+//	cin >> first_name; // reading a string
+//	cin >> age; // reading an integer
+//	cout << "Hello, " << first_name << ", your age is " << age << " !\n";
+//	return 0;
+//}
+
+// 2nd version
+//int main()
+//{
+//	cout << "Hello, enter name and age:\n";
+//	string f_n = "???";
+//	int age = -1;
+//	cin >> f_n >> age;
+//	cout << "Hello, " << f_n << "( age " << age << ")!\n";
+//}
+
+// example
 int main()
 {
-	cout << "Hello, Enter your name and age:\n";
-	string first_name; // string variable definition
-	int age; // integer variable definition
-	cin >> first_name; // reading a string
-	cin >> age; // reading an integer
-	cout << "Hello, " << first_name << ", your age is " << age << " !\n";
-	return 0;
+	cout << "Please enter your name and age:\n";
+	string f_n;
+	double age;
+	cin >> f_n >> age;
+	cout << "Your name is " << f_n << " and you are " << age * 12 << " months old!";
 }
+
+// Operations and operators
+
